df73c833848dc05b5654240925e59c08e9fce19b
ball <1037438310@qq.com>
2022-08-24 13:11:25 +0800

    转换逻辑思考
    转换逻辑思考

--- leet_code_test_project/leet_code_test_project/leet_code_test_project.cpp
+++ leet_code_test_project/leet_code_test_project/leet_code_test_project.cpp
@@ -28,9 +28,18 @@ public:
 
         for (int i = 0; i < s.size(); ++i) {
             //取值在 a - z 之间
-
+            //int c = cur[i] - 'a';
+            //c = ((c + res[i]) % 26 + 26) % 26;
+            //cur[i] = (char)(c + 'a');
+            if (diff_result[i] < 'a') {
+                s[i] = 'z' + (diff_result[i] - 'a' + 1) % 26;
+            }
+            else if (diff_result[i] > 'z') {
+                s[i] = 'a' + (diff_result[i] - 'z' - 1) % 26;
+            }
+            else
+                s[i] = diff_result[i];
         }
-
         return s;
     }
 };
@@ -41,8 +50,8 @@ int main() {
     //string data;
     //help_interface::ReadTextFile(test_path, data);
     //help_interface::StringDataToArrayData(data, nums1);
-    string str = "abc";
-    vector<vector<int>> in_arr = { {0,1,0},{1,2,1},{0,2,1} };
+    string str = "xuwdbdqik";
+    vector<vector<int>> in_arr = { {4, 8, 0}, {4, 4, 0}, {2, 4, 0}, {2, 4, 0}, {6, 7, 1}, {2, 2, 1}, {0, 2, 1}, {8, 8, 0}, {1, 3, 1} };
     Solution s;
     s.shiftingLetters(str, in_arr);
     return 0;
