160d803f421d6ea999336aa1e5eabcfa4ffba86e
ball <1037438310@qq.com>
2022-09-24 22:08:10 +0800

    leetcode球赛T3
    leetcode球赛T3
    dfs+考虑边界

--- leet_code_test_project/leet_code_test_project/leet_code_test_project.cpp
+++ leet_code_test_project/leet_code_test_project/leet_code_test_project.cpp
@@ -1,6 +1,7 @@
 ﻿#include<string>
 #include<map>
 #include<unordered_map>
+#include<unordered_set>
 #include<algorithm>
 #include<assert.h>
 #include<iostream>
@@ -23,21 +24,57 @@ using namespace math_interface;
 
 class Solution {
 public:
-    vector<int> smallestSubarrays(vector<int>& nums) {
-        vector<int> ans(nums.size(), 0);
-        vector<pair<int, int>> ors; //存储最大的位或值 + 对应的最小索引
-        for (int i = nums.size() - 1; i >= 0; --i) {
-            ors.emplace_back(0, i);
-            ors[0].first |= nums[i];
-            int k = 0;
-            for (int j = 1; j < ors.size(); ++j) {
-                ors[j].first |= nums[i];
-                if (ors[j].first == ors[k].first)
-                    ors[k].second = ors[j].second;
-                else ors[k++] = ors[j];
+    vector<vector<int>> ballGame(int num, vector<string>& plate) {
+        int row = plate.size(), col = plate[0].size();
+        vector<vector<int>> ans;
+        function<void(int, int, int, int, int, bool&, int, int)> dfs = [&](
+            int fi, int fj, int ci, int cj, int depth, bool& find, int i, int j) {
+            if (ci < 0 || ci >= row || cj < 0 || cj >= col || depth > num || find)
+                return;
+            int dx = fi - ci, dy = fj - cj;
+            if (plate[ci][cj] == 'O' && depth <= num) {
+                ans.push_back({ i, j });
+                find = true;
             }
-            ors.resize(static_cast<size_t>(k) + 1);
-            ans[i] = ors[0].second - i + 1;
+            else if (plate[ci][cj] == '.') {
+                if (dx == -1) dfs(ci, cj, ci + 1, cj, depth + 1, find, i, j);
+                else if (dx == 1)dfs(ci, cj, ci - 1, cj, depth + 1, find, i, j);
+                else if (dy == -1)dfs(ci, cj, ci, cj + 1, depth + 1, find, i, j);
+                else if (dy == 1)  dfs(ci, cj, ci, cj - 1, depth + 1, find, i, j);
+            }
+            else if (plate[ci][cj] == 'W') {
+                if (dx == -1) dfs(ci, cj, ci, cj + 1, depth + 1, find, i, j);
+                else if (dx == 1)dfs(ci, cj, ci, cj - 1, depth + 1, find, i, j);
+                else if (dy == -1)dfs(ci, cj, ci - 1, cj, depth + 1, find, i, j);
+                else if (dy == 1)  dfs(ci, cj, ci + 1, cj, depth + 1, find, i, j);
+            }
+            else if (plate[ci][cj] == 'E') {
+                if (dx == -1) dfs(ci, cj, ci, cj - 1, depth + 1, find, i, j);
+                else if (dx == 1) dfs(ci, cj, ci, cj + 1, depth + 1, find, i, j);
+                else if (dy == -1) dfs(ci, cj, ci + 1, cj, depth + 1, find, i, j);
+                else if (dy == 1)dfs(ci, cj, ci - 1, cj, depth + 1, find, i, j);
+            }
+        };
+
+        for (int i = 1; i < row - 1; ++i) {
+            if (plate[i][0] == '.') {
+                bool find = false;
+                dfs(i, -1, i, 0, 0, find, i, 0);
+            }
+            if (plate[i][col - 1] == '.') {
+                bool find = false;
+                dfs(i, col, i, col - 1, 0, find, i, col - 1);
+            }
+        }
+        for (int j = 1; j < col - 1; ++j) {
+            if (plate[0][j] == '.') {
+                bool find = false;
+                dfs(-1, j, 0, j, 0, find, 0, j);
+            }
+            if (plate[row - 1][j] == '.') {
+                bool find = false;
+                dfs(row, j, row - 1, j, 0, find, row - 1, j);
+            }
         }
         return ans;
     }
@@ -48,8 +85,9 @@ int main() {
     //string data;
     //help_interface::ReadTextFile(test_path, data);
     //help_interface::StringDataToArrayData(data, nums1);
-    vector<int> nums = { 1,0,2,1,3 };
+    //vector<string> ball = { "..E.",".EOW","..W." };
+    vector<string> ball = { ".....","..E..",".WO..","....." };
     Solution s;
-    s.smallestSubarrays(nums);
+    s.ballGame(5, ball);
     return 0;
 }
