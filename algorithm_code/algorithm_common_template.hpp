3b139d7367a6147033611c11b6a618b367a67a82
ballplayer1992 <1037438310@qq.com>
2022-10-05 22:54:33 +0800

    树状数组模板

--- algorithm_code/algorithm_common_template.hpp
+++ algorithm_code/algorithm_common_template.hpp
@@ -309,6 +309,35 @@ namespace binary_interface {
         }
         return ans;
     }
+
+    int static LowBit(int i) {
+        return i & -i;//(等效于 i - i & (i - 1))
+    }
+    //https://blog.csdn.net/ls2868916989/article/details/119268741
+    //树状数组或二叉索引树（Binary Indexed Tree），
+    //又以其发明者命名为 Fenwick 树。其初衷是解决数据压缩里的累积频率的计算问题，
+    //现多用于高效计算数列的前缀和、区间和。它可以以 O(logn) 的时间得到任意前缀和。
+    //并同时支持在 O(logn) 时间内支持动态单点值的修改。空间复杂度 O(n)。
+    class BIT {
+    private:
+        vector<int> tree;
+        BIT(int n):tree(n){}
+        void Update(int i, int val) { //单点更新
+            while (i < tree.size()) {
+                tree[i] += val;
+                i += LowBit(i);
+            }
+        }
+        int Query(int i) {//求区间[1,i]内所有元素和
+            int value = 0;
+            while (i > 0) {
+                value += tree[i];
+                i -= LowBit(i);
+            }
+            return value;
+        }
+    };
+
 }
 
 namespace data_struct_interface {
